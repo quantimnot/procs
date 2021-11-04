83a2c0525f1d384a69f283f2799b774e33d74023
quantimnot <quantimnot@users.noreply.github.com>
2021-11-04 17:57:32 -0400

    Added boost_prccess to list of impls for study

--- procs.nim
+++ procs.nim
@@ -7,6 +7,7 @@
 #      - https://github.com/cheatfate/asynctools/blob/master/asynctools/asyncproc.nim
 #      - https://github.com/disruptek/golden/blob/master/src/golden/invoke.nim
 #      - https://github.com/yglukhov/asyncthreadpool/blob/main/asyncthreadpool/private/pipes.nim
+#      - https://www.boost.org/doc/libs/1_77_0/doc/html/boost_process/tutorial.html
 #
 #    - Keep these issues in mind:
 #      - https://github.com/status-im/nim-chronos/issues/77
