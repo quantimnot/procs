fd76f8815f705e0e69dd7374222daeb0f62c0c20
quantimnot <quantimnot@users.noreply.github.com>
2021-11-04 17:54:58 -0400

    Add initial impl file

---
+++ procs.nim
@@ -0,0 +1,14 @@
+## # Procs
+##
+## ## Features
+##
+#  TODO:
+#    - Learn from these:
+#      - https://github.com/cheatfate/asynctools/blob/master/asynctools/asyncproc.nim
+#      - https://github.com/disruptek/golden/blob/master/src/golden/invoke.nim
+#      - https://github.com/yglukhov/asyncthreadpool/blob/main/asyncthreadpool/private/pipes.nim
+#
+#    - Keep these issues in mind:
+#      - https://github.com/status-im/nim-chronos/issues/77
+#      - https://github.com/nim-lang/Nim/labels/osproc
+
